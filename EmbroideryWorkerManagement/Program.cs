069c12a7dd0a23b75849b1290fd5b30ae72fee09
Hasib Mahmud <Hasib8920@gmail.com>
2026-08-18 12:05:15 +0600

    Configure application to use dynamic port from environment

--- EmbroideryWorkerManagement/Program.cs
+++ EmbroideryWorkerManagement/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
